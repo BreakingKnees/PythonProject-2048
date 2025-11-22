716f0eab46393ee07869702162a894c7d5ca800a
Aaditya Khanna <ak.aadityakhanna@gmail.com>
2025-11-22 23:19:03 +0530

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -5,10 +5,15 @@ def load_highscore():
     global highscore
     try:
         with open(SCORE_FILE, "r") as f:
-            highscore = int(f.read())
+            content = f.read().strip()
+            if content == "":
+                highscore = 0
+            else:
+                highscore = int(content)
     except:
         highscore = 0
 
 def save_highscore():
     with open(SCORE_FILE, "w") as f:
         f.write(str(highscore))
+
